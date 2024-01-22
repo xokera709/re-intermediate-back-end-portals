5f6f5551eb4f3afde08f3ffa85cd9b3ae779b267
xokera709 <148460685+xokera709@users.noreply.github.com>
2024-01-22 15:23:09 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,17 @@
+        data.push(Math.floor(Math.random() * 100) + 1);
+        console.log("Random Number: " + item);
+main();
+
+    for (let i = 0; i < 10; i++) {
+function main() {
+    }
+    });
+function generateRandomData() {
+
+}
+    const data = [];
+    data.forEach(item => {
+
+    const data = generateRandomData();
+}
+    return data;
